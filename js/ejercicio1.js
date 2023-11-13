933ebc846d825dd720fea6729ed018fbb193c855
Florencia Gonzalez <iturbe.mariaflorencia@gmail.com>
2023-11-13 08:44:27 -0300

    agregue el ejercicio 1

---
+++ js/ejercicio1.js
@@ -0,0 +1,10 @@
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+document.write('<h1>Lista de meses</h2>')
+document.write('<ul>')
+
+for (let i = 0; i < meses.length; i++) {
+    document.write(`<li>${meses[i]}</li>`)
+    
+}
+document.write('</ul>')
